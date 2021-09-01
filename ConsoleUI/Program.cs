881a767dd734388157ede9567b2dedb1ad7dd02f
Bilal Bora <bilal.bora@ogr.sakarya.edu.tr>
2021-09-01 03:00:22 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            
+            carManager.Add(new Car { Id = 6, BrandId = 3, ColorId = 2, DailyPrice = 40000, Description = "SUV", ModelYear = 2004 });
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("Car price = " + car.DailyPrice + " TL'dir." + car.Description + " türündedir." );
+            }
+
+        }
+    }
+}
